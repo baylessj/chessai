8d4c7771a7123383ec83ce14b2028ad1f7b8f141
Jonathan Bayless <baylessj@purdue.edu>
2017-09-20 17:21:11 -0400

    Attempting c++ with Visual Studio

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "test\n";
+    return 0;
+}
+
