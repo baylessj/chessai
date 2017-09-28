ceb4320ae1fa5dfffbd7addb1cf1dc5ca33fdd9f
Jonathan Bayless <baylessj@purdue.edu>
2017-09-28 18:28:40 -0400

    CLI code is properly commented now

--- ConsoleApplication1/cli.cpp
+++ ConsoleApplication1/cli.cpp
@@ -130,6 +130,14 @@ static void displayMove(Move &move) {
 	return;
 }
 
+/**
+* @brief Executes the commands parsed by the readCommands() function
+*
+* @param buf
+*        The string designating which command to run
+*
+* @return True when the command executes properly
+*/
 bool CLI::doCommand(const char *buf) {
 	Move move;
 	char userinput[80];
@@ -361,6 +369,9 @@ bool CLI::doCommand(const char *buf) {
 	return true;
 }
 
+/**
+* @brief Displays info about the current game
+*/
 void CLI::info()
 {
 
@@ -381,243 +392,9 @@ void CLI::info()
 	return;
 }
 
-bool FEN::readFen(char *filename, int number)
-{
-	int numberf;
-	char s[180];
-	char fenwhite[80];
-	char fenblack[80];
-	char fen[100];
-	char fencolor[2];
-	char fencastling[5];
-	char fenenpassant[3];
-	char temp[80];
-	int fenhalfmoveclock;
-	int fenfullmovenumber;
-	bool returnValue;
-	FILE * fp;
-
-	returnValue = false;
-	if (number <= 0) return returnValue;
-
-	// open the file for read and scan through until we find the number-th position:
-	fp = fopen(filename, "rt");
-	if (fp != NULL)
-	{
-		numberf = 0;
-		while (fscanf(fp, "%s", s) != EOF)
-		{
-			if (!strcmp(s, "[White"))
-			{
-				fscanf(fp, "%s", fenwhite);
-				// remove first (") and last two characters ("]) from fenwhite:
-				strcpy(temp, "");
-				strncat(temp, fenwhite, strlen(fenwhite) - 2);
-				strcpy(temp, temp + 1);
-				strcpy(fenwhite, temp);
-			}
-			if (!strcmp(s, "[Black"))
-			{
-				fscanf(fp, "%s", fenblack);
-				// remove first (") and last two characters ("]) from fenblack:
-				strcpy(temp, "");
-				strncat(temp, fenblack, strlen(fenblack) - 2);
-				strcpy(temp, temp + 1);
-				strcpy(fenblack, temp);
-			}
-			if (!strcmp(s, "[FEN"))
-			{
-				// position found, so increment numberf.
-				// we already have fenwhite and fenblack.
-				numberf++;
-				if (numberf == number)
-				{
-					fscanf(fp, "%s", fen);
-					fscanf(fp, "%s", fencolor);           // b or w
-					fscanf(fp, "%s", fencastling);        // -, or KQkq
-					fscanf(fp, "%s", fenenpassant);       // -, or e3, or b6, etc
-					fscanf(fp, "%d", &fenhalfmoveclock);  // int, used for the fifty move draw rule
-					fscanf(fp, "%d", &fenfullmovenumber); // int. start with 1, It is incremented after move by Black
-
-					cout << endl << "winglet> fen #" << numberf << " in " << filename << ":" << endl << endl;
-					cout << " White: " << fenwhite << endl;
-					cout << " Black: " << fenblack << endl;
-					cout << " " << &fen[1] << endl;
-					if (fencolor[0] == 'w')
-					{
-						cout << " wt to move next" << endl;
-					}
-					else
-					{
-						cout << " bl to move next" << endl;
-					}
-					cout << " Castling: " << fencastling << endl;
-					cout << " EP square: " << fenenpassant << endl;
-					cout << " Fifty move count: " << fenhalfmoveclock << endl;
-					cout << " Move number: " << fenfullmovenumber << endl << endl;
-				}
-			}
-		}
-
-		if (numberf < number)
-		{
-			printf("winglet> only %d fens present in %s, fen #%d not found\n",
-				numberf, filename, number);
-			returnValue = false;
-		}
-		else
-		{
-			setupFen(fen, fencolor, fencastling, fenenpassant, fenhalfmoveclock, fenfullmovenumber);
-			returnValue = true;
-		}
-		fclose(fp);
-	}
-	else
-	{
-		printf("winglet> error opening file: %s\n", filename);
-		returnValue = false;
-	}
-	return returnValue;
-}
-
-void FEN::setupFen(char *fen, char *fencolor, char *fencastling, char *fenenpassant, int fenhalfmoveclock, int fenfullmovenumber)
-{
-	int i, file, rank, counter, piece;
-	int whiteCastle, blackCastle, next, epsq;
-
-	piece = 0;
-	for (i = 0; i < 64; i++)
-	{
-		board.square[i] = EMPTY;
-	}
-
-	// loop over the FEN string characters, and populate board.square[]
-	// i is used as index for the FEN string
-	// counter is the index for board.square[], 0..63
-	// file and rank relate to the position on the chess board, 1..8
-	// There is no error/legality checking on the FEN string!!
-	file = 1;
-	rank = 8;
-	i = 0;
-	counter = 0;
-	while ((counter < 64) && (fen[i] != '\0'))
-	{
-		// '1'  through '8':
-		if (((int)fen[i] > 48) && ((int)fen[i] < 57))
-		{
-			file += (int)fen[i] - 48;
-			counter += (int)fen[i] - 48;
-		}
-		else
-			//  other characters:
-		{
-			switch (fen[i])
-			{
-			case '/':
-				rank--;
-				file = 1;
-				break;
-
-			case 'P':
-				board.square[BOARDINDEX[file][rank]] = WHITE_PAWN;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'N':
-				board.square[BOARDINDEX[file][rank]] = WHITE_KNIGHT;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'B':
-				board.square[BOARDINDEX[file][rank]] = WHITE_BISHOP;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'R':
-				board.square[BOARDINDEX[file][rank]] = WHITE_ROOK;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'Q':
-				board.square[BOARDINDEX[file][rank]] = WHITE_QUEEN;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'K':
-				board.square[BOARDINDEX[file][rank]] = WHITE_KING;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'p':
-				board.square[BOARDINDEX[file][rank]] = BLACK_PAWN;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'n':
-				board.square[BOARDINDEX[file][rank]] = BLACK_KNIGHT;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'b':
-				board.square[BOARDINDEX[file][rank]] = BLACK_BISHOP;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'r':
-				board.square[BOARDINDEX[file][rank]] = BLACK_ROOK;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'q':
-				board.square[BOARDINDEX[file][rank]] = BLACK_QUEEN;
-				file += 1;
-				counter += 1;
-				break;
-
-			case 'k':
-				board.square[BOARDINDEX[file][rank]] = BLACK_KING;
-				file += 1;
-				counter += 1;
-				break;
-
-			default:
-				break;
-			}
-		}
-		i++;
-	}
-	next = WHITE_MOVE;
-	if (fencolor[0] == 'b') next = BLACK_MOVE;
-
-	whiteCastle = 0;
-	blackCastle = 0;
-	if (strstr(fencastling, "K")) whiteCastle += CANCASTLEOO;
-	if (strstr(fencastling, "Q")) whiteCastle += CANCASTLEOOO;
-	if (strstr(fencastling, "k")) blackCastle += CANCASTLEOO;
-	if (strstr(fencastling, "q")) blackCastle += CANCASTLEOOO;
-	if (strstr(fenenpassant, "-"))
-	{
-		epsq = 0;
-	}
-	else
-	{
-		// translate a square coordinate (as string) to int (eg 'e3' to 20):
-		epsq = ((int)fenenpassant[0] - 96) + 8 * ((int)fenenpassant[1] - 48) - 9;
-	}
-	board.initFromSquares(board.square, next, fenhalfmoveclock, whiteCastle, blackCastle, epsq);
-
-}
-
+/**
+* @brief Allows for the modification of the board setup for a new game
+*/
 void CLI::setup()
 {
 	// interactively setup the chess board
@@ -880,3 +657,273 @@ void CLI::setup()
 
 	}
 }
+
+/**
+*
+* The FEN functions are all direct implementations of the FEN initialization code written for the Winglet Chess Engine.
+*
+*/
+
+/**
+* @brief Interprets a Forsyth-Edwards Notation setup for the board
+*
+* @param filename
+*        FEN file to open
+* @param number
+*        The position number to be initialized from the FEN file
+*
+* @return True when the FEN reading properly executes
+*/
+bool FEN::readFen(char *filename, int number)
+{
+	int numberf;
+	char s[180];
+	char fenwhite[80];
+	char fenblack[80];
+	char fen[100];
+	char fencolor[2];
+	char fencastling[5];
+	char fenenpassant[3];
+	char temp[80];
+	int fenhalfmoveclock;
+	int fenfullmovenumber;
+	bool returnValue;
+	FILE * fp;
+
+	returnValue = false;
+	if (number <= 0) return returnValue;
+
+	// open the file for read and scan through until we find the number-th position:
+	fp = fopen(filename, "rt");
+	if (fp != NULL)
+	{
+		numberf = 0;
+		while (fscanf(fp, "%s", s) != EOF)
+		{
+			if (!strcmp(s, "[White"))
+			{
+				fscanf(fp, "%s", fenwhite);
+				// remove first (") and last two characters ("]) from fenwhite:
+				strcpy(temp, "");
+				strncat(temp, fenwhite, strlen(fenwhite) - 2);
+				strcpy(temp, temp + 1);
+				strcpy(fenwhite, temp);
+			}
+			if (!strcmp(s, "[Black"))
+			{
+				fscanf(fp, "%s", fenblack);
+				// remove first (") and last two characters ("]) from fenblack:
+				strcpy(temp, "");
+				strncat(temp, fenblack, strlen(fenblack) - 2);
+				strcpy(temp, temp + 1);
+				strcpy(fenblack, temp);
+			}
+			if (!strcmp(s, "[FEN"))
+			{
+				// position found, so increment numberf.
+				// we already have fenwhite and fenblack.
+				numberf++;
+				if (numberf == number)
+				{
+					fscanf(fp, "%s", fen);
+					fscanf(fp, "%s", fencolor);           // b or w
+					fscanf(fp, "%s", fencastling);        // -, or KQkq
+					fscanf(fp, "%s", fenenpassant);       // -, or e3, or b6, etc
+					fscanf(fp, "%d", &fenhalfmoveclock);  // int, used for the fifty move draw rule
+					fscanf(fp, "%d", &fenfullmovenumber); // int. start with 1, It is incremented after move by Black
+
+					cout << endl << "winglet> fen #" << numberf << " in " << filename << ":" << endl << endl;
+					cout << " White: " << fenwhite << endl;
+					cout << " Black: " << fenblack << endl;
+					cout << " " << &fen[1] << endl;
+					if (fencolor[0] == 'w')
+					{
+						cout << " wt to move next" << endl;
+					}
+					else
+					{
+						cout << " bl to move next" << endl;
+					}
+					cout << " Castling: " << fencastling << endl;
+					cout << " EP square: " << fenenpassant << endl;
+					cout << " Fifty move count: " << fenhalfmoveclock << endl;
+					cout << " Move number: " << fenfullmovenumber << endl << endl;
+				}
+			}
+		}
+
+		if (numberf < number)
+		{
+			printf("winglet> only %d fens present in %s, fen #%d not found\n",
+				numberf, filename, number);
+			returnValue = false;
+		}
+		else
+		{
+			setupFen(fen, fencolor, fencastling, fenenpassant, fenhalfmoveclock, fenfullmovenumber);
+			returnValue = true;
+		}
+		fclose(fp);
+	}
+	else
+	{
+		printf("winglet> error opening file: %s\n", filename);
+		returnValue = false;
+	}
+	return returnValue;
+}
+
+/**
+* @brief Sets up the board according to the FEN strings found in readFEN()
+*
+* @param fen
+*        The FEN string designating the position setup
+* @param fencolor
+*        The color of the next move to be played in the game being initialized
+* @param fencastling
+*        Designates if the FEN position being evaluated represents a castle
+* @param fenenpassant
+*        Designates if the FEN position being evaluated represents an en passant capture
+* @param fenhalfmoveclock
+*        Fifty Move number
+* @param fenfullmovenumber
+*        Doesn't actually do anything...
+*/
+void FEN::setupFen(char *fen, char *fencolor, char *fencastling, char *fenenpassant, int fenhalfmoveclock, int fenfullmovenumber)
+{
+	int i, file, rank, counter, piece;
+	int whiteCastle, blackCastle, next, epsq;
+
+	piece = 0;
+	for (i = 0; i < 64; i++)
+	{
+		board.square[i] = EMPTY;
+	}
+
+	// loop over the FEN string characters, and populate board.square[]
+	// i is used as index for the FEN string
+	// counter is the index for board.square[], 0..63
+	// file and rank relate to the position on the chess board, 1..8
+	// There is no error/legality checking on the FEN string!!
+	file = 1;
+	rank = 8;
+	i = 0;
+	counter = 0;
+	while ((counter < 64) && (fen[i] != '\0'))
+	{
+		// '1'  through '8':
+		if (((int)fen[i] > 48) && ((int)fen[i] < 57))
+		{
+			file += (int)fen[i] - 48;
+			counter += (int)fen[i] - 48;
+		}
+		else
+			//  other characters:
+		{
+			switch (fen[i])
+			{
+			case '/':
+				rank--;
+				file = 1;
+				break;
+
+			case 'P':
+				board.square[BOARDINDEX[file][rank]] = WHITE_PAWN;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'N':
+				board.square[BOARDINDEX[file][rank]] = WHITE_KNIGHT;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'B':
+				board.square[BOARDINDEX[file][rank]] = WHITE_BISHOP;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'R':
+				board.square[BOARDINDEX[file][rank]] = WHITE_ROOK;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'Q':
+				board.square[BOARDINDEX[file][rank]] = WHITE_QUEEN;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'K':
+				board.square[BOARDINDEX[file][rank]] = WHITE_KING;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'p':
+				board.square[BOARDINDEX[file][rank]] = BLACK_PAWN;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'n':
+				board.square[BOARDINDEX[file][rank]] = BLACK_KNIGHT;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'b':
+				board.square[BOARDINDEX[file][rank]] = BLACK_BISHOP;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'r':
+				board.square[BOARDINDEX[file][rank]] = BLACK_ROOK;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'q':
+				board.square[BOARDINDEX[file][rank]] = BLACK_QUEEN;
+				file += 1;
+				counter += 1;
+				break;
+
+			case 'k':
+				board.square[BOARDINDEX[file][rank]] = BLACK_KING;
+				file += 1;
+				counter += 1;
+				break;
+
+			default:
+				break;
+			}
+		}
+		i++;
+	}
+	next = WHITE_MOVE;
+	if (fencolor[0] == 'b') next = BLACK_MOVE;
+
+	whiteCastle = 0;
+	blackCastle = 0;
+	if (strstr(fencastling, "K")) whiteCastle += CANCASTLEOO;
+	if (strstr(fencastling, "Q")) whiteCastle += CANCASTLEOOO;
+	if (strstr(fencastling, "k")) blackCastle += CANCASTLEOO;
+	if (strstr(fencastling, "q")) blackCastle += CANCASTLEOOO;
+	if (strstr(fenenpassant, "-"))
+	{
+		epsq = 0;
+	}
+	else
+	{
+		// translate a square coordinate (as string) to int (eg 'e3' to 20):
+		epsq = ((int)fenenpassant[0] - 96) + 8 * ((int)fenenpassant[1] - 48) - 9;
+	}
+	board.initFromSquares(board.square, next, fenhalfmoveclock, whiteCastle, blackCastle, epsq);
+
+}
+
--- ConsoleApplication1/cli.h
+++ ConsoleApplication1/cli.h
@@ -14,9 +14,10 @@ public:
 	CLI();
 	bool doCommand(const char *);
 	void readCommands();
-	void info();
-	void setup();
+
 private:
+	void setup();
+	void info();
 	void init();
 	static const int MAX_CMD_BUFF = 256; // Console command input buffer
 	char CMD_BUFF[MAX_CMD_BUFF];
